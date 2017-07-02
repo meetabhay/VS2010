95d06b9b9e860c37d9901b614625fa54a679189c
Abhay Bhardwaj <meetabhaybhardwaj@gmail.com>
2017-07-02 18:45:55 +0530

    First Git Repo VS Check-In

---
+++ MVC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Git Repo from VS 2010");
+        }
+    }
+}
