b0c964c1363a8dcac115d25ab0e591951d9396b1
Abhay Bhardwaj <meetabhaybhardwaj@gmail.com>
2017-07-02 18:53:17 +0530

    NewBranch Changes

---
+++ MVC/Bundle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVC
+{
+    class Bundle
+    {
+        public void MethodName()
+        {
+            Console.WriteLine("Develop-Major Branch - New Class");
+        }
+    }
+}
